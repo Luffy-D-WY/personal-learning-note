fbf815f8a7106f241c2e7be5fda3319b65885d05
Wuyou <1316197346@qq.com>
2025-10-10 16:54:55 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,70 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "../utils/auto_sidebar.mjs";	// 改成自己的路径
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/icon.png" }]],
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outlineTitle:'目录',
+    outline:[2,6], // 目录层级
+    nav: [
+      { text: 'Home', items:[
+        {text:'api-examples', link: '/api-examples' },
+        {text:'markdown-examples', link: '/markdown-examples' }
+      ] },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+    logo: "icon.png", // 配置logo位置，public目录
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    sidebar: false,
+    aside:"left",
+
+    socialLinks: [
+      { 
+        icon: 'github', 
+        link: 'https://github.com/Luffy-D-WY' 
+      },
+      { 
+        icon: {
+          svg: '<svg height="1em" style="flex:none;line-height:1" viewBox="0 0 24 24" width="1em" xmlns="http://www.w3.org/2000/svg"><title>bilibili</title><path clip-rule="evenodd" d="M4.977 3.561a1.31 1.31 0 111.818-1.884l2.828 2.728c.08.078.149.163.205.254h4.277a1.32 1.32 0 01.205-.254l2.828-2.728a1.31 1.31 0 011.818 1.884L17.82 4.66h.848A5.333 5.333 0 0124 9.992v7.34a5.333 5.333 0 01-5.333 5.334H5.333A5.333 5.333 0 010 17.333V9.992a5.333 5.333 0 015.333-5.333h.781L4.977 3.56zm.356 3.67a2.667 2.667 0 00-2.666 2.667v7.529a2.667 2.667 0 002.666 2.666h13.334a2.667 2.667 0 002.666-2.666v-7.53a2.667 2.667 0 00-2.666-2.666H5.333zm1.334 5.192a1.333 1.333 0 112.666 0v1.192a1.333 1.333 0 11-2.666 0v-1.192zM16 11.09c-.736 0-1.333.597-1.333 1.333v1.192a1.333 1.333 0 102.666 0v-1.192c0-.736-.597-1.333-1.333-1.333z" fill="#FB7299" fill-rule="evenodd"></path></svg>'
+        },
+        link: 'https://space.bilibili.com/1147169736?spm_id_from=333.1007.0.0'
+      }
+    ],
+
+    footer: {
+      copyright: 'Copyright © 2025 WY',
+    },
+       // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  }
+  
+})
